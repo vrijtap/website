22627b77fba992e8b1118e01911c36b94f92fa8e
brian-vdb <brianvdberg2107@gmail.com>
2024-01-23 19:28:30 +0100

    Improved the structure of the owner router

--- api/routers/owner.go
+++ api/routers/owner.go
@@ -12,15 +12,10 @@ import (
 func ConfigureOwnerRoutes(router *mux.Router) {
 	// Create a subrouter for owner-related routes under the "/owner" path.
 	ownerRouter := router.PathPrefix("/owner").Subrouter()
-
 	ownerRouter.Use(middleware.AuthenticationMiddleware)
 
-	// Handle GET requests to the "/owner" endpoint with the OwnerGET handler function.
+	// Define routes for owner-related endpoints
 	ownerRouter.HandleFunc("", handlers.OwnerGet).Methods(http.MethodGet)
-
-	// Handle POST requests to the "/owner" endpoint with the OwnerLogin handler function.
 	ownerRouter.HandleFunc("", handlers.OwnerLogin).Methods(http.MethodPost)
-
-	// Handle PUT requests to the "/owner" endpoint with the OwnerPUT handler function.
 	ownerRouter.HandleFunc("", handlers.OwnerPut).Methods(http.MethodPut)
 }
